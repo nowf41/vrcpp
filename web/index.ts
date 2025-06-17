3aae25818211467d194194eae6a0408cebf96ccb
nowf41 <111325821+nowf41@users.noreply.github.com>
2025-06-17 20:21:01 +0900

    REF: add router

--- web/index.ts
+++ web/index.ts
@@ -7,6 +7,9 @@ app.set('views', './views');
 app.set('view engine', 'pug');
 app.use(express.static('./static'));
 
+// routes
+app.use('/', require('./routes/rootRenderer'));
+
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server started at http://localhost:${process.env.PORT || 3000}`);
 })
