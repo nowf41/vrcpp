ef542bb65b707840b0e3789f041856ebb4a8b4ca
nowf41 <111325821+nowf41@users.noreply.github.com>
2025-06-17 20:17:42 +0900

    ADD web

---
+++ web/index.ts
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+// template engine
+app.set('views', './views');
+app.set('view engine', 'pug');
+app.use(express.static('./static'));
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server started at http://localhost:${process.env.PORT || 3000}`);
+})
